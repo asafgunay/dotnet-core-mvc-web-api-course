af282e357fa8c2eccfd1d337828b96f35c6da110
Asaf Günay <asaf.gunay@windowslive.com>
2019-04-21 13:38:47 +0300

    startup configured for npm

--- OOPTut/src/OOPTut.Web.UI/Startup.cs
+++ OOPTut/src/OOPTut.Web.UI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace OOPTut.Web.UI
 {
@@ -29,6 +31,13 @@ namespace OOPTut.Web.UI
             {
                 app.UseDeveloperExceptionPage();
             }
+            // statik dosyalarin kullanimini acar herkese!
+            // Ayrica new StaticFileOptions() ile wwwroot disinda kalan node_modules klasorunu /vendor path'i ile cagirmamizi ayarliyoruz.
+            app.UseStaticFiles(new StaticFileOptions() {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath = new PathString("/vendor")
+            });
 
             app.UseMvc(routes =>
             {

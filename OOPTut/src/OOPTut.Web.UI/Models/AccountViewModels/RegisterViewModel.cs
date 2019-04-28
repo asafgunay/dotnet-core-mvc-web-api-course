69e81f4db39b45c72eab06b2de88e16fa9cadb83
Asaf Günay <asaf.gunay@windowslive.com>
2019-04-28 12:58:07 +0300

    regıstervıewmodel created

---
+++ OOPTut/src/OOPTut.Web.UI/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOPTut.Web.UI.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Eposta")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Parola")]
+        [StringLength(20, ErrorMessage = "Lutfen {0} en az {2} karakter olmali en uzun {1} karakter olmali", MinimumLength = 6)]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Parola Doğrula")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Şifreler aynı değil!")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name = "Ad")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Soyad")]
+        public string LastName { get; set; }
+    }
+}

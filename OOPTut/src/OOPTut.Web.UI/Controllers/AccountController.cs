1a5f924517bb8696ab20ffeea969512c78e4662e
Asaf Günay <asaf.gunay@windowslive.com>
2019-04-28 13:15:28 +0300

    accountcontroller fixed

--- OOPTut/src/OOPTut.Web.UI/Controllers/AccountController.cs
+++ OOPTut/src/OOPTut.Web.UI/Controllers/AccountController.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OOPTut.Core.Users;
 using OOPTut.Web.UI.Models;
+using OOPTut.Web.UI.Models.AccountViewModels;
 
 namespace OOPTut.Web.UI.Controllers
 {
+    // Microsoft.AspNetCore.Identity
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -22,10 +34,30 @@ namespace OOPTut.Web.UI.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Register(RegisterModel registerModel)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
             // kayit etme islemleri
-            return View();
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailConfirmed=true,
+                    TwoFactorEnabled=false
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Contact", "Home");
+                }
+
+                return View();
+            }
         }
     }
 }

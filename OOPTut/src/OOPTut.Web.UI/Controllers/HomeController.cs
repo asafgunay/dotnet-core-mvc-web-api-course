271623e5ca924eb1723bd4575864bf507f91f8d2
Asaf Günay <asaf.gunay@windowslive.com>
2019-04-28 13:43:26 +0300

    register cshtml updated

--- OOPTut/src/OOPTut.Web.UI/Controllers/HomeController.cs
+++ OOPTut/src/OOPTut.Web.UI/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OOPTut.Web.UI.Controllers
@@ -16,6 +17,7 @@ namespace OOPTut.Web.UI.Controllers
         {
             return View();
         }
+        [Authorize]
         public IActionResult Contact()
         {
             return View();

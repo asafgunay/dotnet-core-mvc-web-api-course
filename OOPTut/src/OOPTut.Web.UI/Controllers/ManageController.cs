27a45cc18f07f0d4bdbbd6511a87a144b03f7485
Asaf Günay <asaf.gunay@windowslive.com>
2019-05-26 14:22:38 +0300

    ManageController created Index.cshtml created

---
+++ OOPTut/src/OOPTut.Web.UI/Controllers/ManageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OOPTut.Core.Users;
+
+namespace OOPTut.Web.UI.Controllers
+{
+    public class ManageController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ManageController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+    }
+}

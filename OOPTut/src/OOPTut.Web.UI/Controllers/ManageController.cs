9cf495b5c38f49c477ff5b2102d09331acc5269e
Asaf Günay <asaf.gunay@windowslive.com>
2019-05-26 15:38:49 +0300

    Sezon finali :)

--- OOPTut/src/OOPTut.Web.UI/Controllers/ManageController.cs
+++ OOPTut/src/OOPTut.Web.UI/Controllers/ManageController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,16 +21,27 @@ namespace OOPTut.Web.UI.Controllers
             _userManager = userManager;
             _roleManager = roleManager;
         }
-
+        // [Authorize(Roles ="Admin, TeamLeader")]
         public async Task<IActionResult> Index()
         {
             var users = await _userManager.Users.ToListAsync();
             return View(users);
         }
+        // [Authorize(Roles ="Admin")]
         public async Task<IActionResult> CreateRole()
         {
             ViewBag.RoleList = await _roleManager.Roles.ToListAsync();
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(IdentityRole model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _roleManager.CreateAsync(model);
+                return RedirectToAction("CreateRole");
+            }
+            return View(model);
         }
 
 

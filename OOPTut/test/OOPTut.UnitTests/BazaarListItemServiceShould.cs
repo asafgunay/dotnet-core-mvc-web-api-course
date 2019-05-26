9707bfe08850ca8ee2294fff231dcc8da4089ddd
Asaf Günay <asaf.gunay@windowslive.com>
2019-05-26 13:53:08 +0300

    Create test completed

--- OOPTut/test/OOPTut.UnitTests/BazaarListItemServiceShould.cs
+++ OOPTut/test/OOPTut.UnitTests/BazaarListItemServiceShould.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using OOPTut.Application.BazaarListItemServices;
+using OOPTut.Application.BazaarListItemServices.Dto;
+using OOPTut.Core.Bazaar;
+using OOPTut.EntityFramework.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +14,52 @@ namespace OOPTut.UnitTests
     public class BazaarListItemServiceShould
     {
         // sabit bir bazaarlist id
+        private readonly int bazaarListId;
         // options
+        private readonly DbContextOptions<ApplicationUserDbContext> options;
         // context
+        private readonly ApplicationUserDbContext inMemoryDbContext;
         // servis
+        private readonly BazaarListItemService service;
+
         // constructor
+        public BazaarListItemServiceShould()
+        {
+            bazaarListId = 1;
+            options = new DbContextOptionsBuilder<ApplicationUserDbContext>().UseInMemoryDatabase(databaseName: "BazaarListItemService_TestDb").Options;
+            inMemoryDbContext = new ApplicationUserDbContext(options);
+            service = new BazaarListItemService(inMemoryDbContext);
+        }
         private async Task CreateFirst()
         {
             // parametresiyle fake data olustur
+            CreateBazaarListItem fakeInput = new CreateBazaarListItem {
+                BazaarListId = bazaarListId,
+                CreatorUserId = Guid.NewGuid().ToString(),
+                Name = "Domates_Test"
+            };
             // servisi calistir
+            await service.CreateAsync(fakeInput);
         }
         private async Task CleanAll()
         {
+            // tum veriyi list halinde getir
+            var getAll = await inMemoryDbContext.BazaarListItems.ToListAsync();
             // ilgili tum veriyi sil
+            inMemoryDbContext.RemoveRange(getAll);
+            await inMemoryDbContext.SaveChangesAsync();
         }
         [Fact]
         public async Task Create()
         {
-
+            await CreateFirst();
             // kaydedilen veriyi cek
+            BazaarListItem created = await inMemoryDbContext.BazaarListItems.FirstOrDefaultAsync();
             // kontrol et!
+            Assert.False(created.IsCanceled);
+            Assert.False(created.IsCompleted);
+            Assert.Equal(bazaarListId, created.BazaarListId);
+            Assert.Equal("Domates_Test", created.Name);
         }
         [Fact]
         public async Task Get()

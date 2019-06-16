6f15a2d321f718db62301af0bab580d6398043c4
Asaf Günay <asaf.gunay@windowslive.com>
2019-06-16 13:29:23 +0300

    NavBarMenuItem db created

--- OOPTut/src/OOPTut.EntityFramework/Contexts/ApplicationUserDbContext.cs
+++ OOPTut/src/OOPTut.EntityFramework/Contexts/ApplicationUserDbContext.cs
@@ -1,18 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OOPTut.Core.Bazaar;
+using OOPTut.Core.Navbar;
 using OOPTut.Core.Users;
 
 namespace OOPTut.EntityFramework.Contexts
 {
     public class ApplicationUserDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationUserDbContext(DbContextOptions<ApplicationUserDbContext> options):base(options)
+        public ApplicationUserDbContext(DbContextOptions<ApplicationUserDbContext> options) : base(options)
         {
         }
 
         // Veritabanina eklenecek tablolar icin tanimlanan model siniflari buraya gelecek:
         public DbSet<BazaarList> BazaarLists { get; set; }
         public DbSet<BazaarListItem> BazaarListItems { get; set; }
+        public DbSet<NavBarMenuItem> NavBarMenuItems { get; set; }
     }
 }
---
+++ OOPTut/src/OOPTut.EntityFramework/Migrations/20190616102451_CreateNavBarMenuTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OOPTut.EntityFramework.Migrations
+{
+    public partial class CreateNavBarMenuTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NavBarMenuItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true),
+                    OpenInSamePage = table.Column<bool>(nullable: false),
+                    Icon = table.Column<string>(nullable: true),
+                    Roles = table.Column<string>(nullable: true),
+                    IsAnonym = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NavBarMenuItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NavBarMenuItems");
+        }
+    }
+}

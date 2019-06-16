0fee61466e6715c78e6f67a971732536168e5a1b
Asaf Günay <asaf.gunay@windowslive.com>
2019-06-16 13:16:18 +0300

    NavBarMenuItem class created

---
+++ OOPTut/src/OOPTut.Core/Navbar/NavBarMenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPTut.Core.Navbar
+{
+    public class NavBarMenuItem
+    {
+        public int Id { get; set; } // 1
+        public string Title { get; set; } // Anasayfa
+        public string Url { get; set; } // Home/Index
+        public bool OpenInSamePage { get; set; } // true
+        public string Icon { get; set; } // "fa fa-home"
+        public string Roles { get; set; } // bos
+        public bool IsAnonym { get; set; } // true
+                                           /*
+                                   <a href="@Url" target="@{OpenInSamePage ? "_self" : "_blank" } title="@Title">
+                                       @if(!string.IsNullOrEmpty(Icon)){
+                                           <span class="@Icon"></span>
+                                       }
+                                       @Title
+                                   </a>
+                                            */
+        public static NavBarMenuItem Create(string title, string url, bool openInSamePage, string icon, string roles)
+        {
+            NavBarMenuItem item = new NavBarMenuItem
+            {
+                Title = title,
+                Url = url,
+                OpenInSamePage = openInSamePage,
+                Icon = icon,
+                Roles = roles,
+                IsAnonym = string.IsNullOrEmpty(roles)
+            };
+            return item;
+        }
+    }
+}
---
+++ OOPTut/src/OOPTut.Web.UI/ViewComponents/NavBarMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOPTut.Web.UI.ViewComponents
+{
+    [ViewComponent]
+    public class NavBarMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

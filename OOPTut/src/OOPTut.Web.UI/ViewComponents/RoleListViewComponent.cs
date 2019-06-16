32470e4180442253b23805af435aded2528dbabe
Asaf Günay <asaf.gunay@windowslive.com>
2019-06-16 10:52:14 +0300

    RoleListViewComponent Created

---
+++ OOPTut/src/OOPTut.Web.UI/ViewComponents/RoleListViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace OOPTut.Web.UI.ViewComponents
+{
+    [ViewComponent]
+    public class RoleListViewComponent : ViewComponent
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleListViewComponent(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<IdentityRole> roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+    }
+}

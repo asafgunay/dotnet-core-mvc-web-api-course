7348329834dd9411b0d0c85f2599f34e0de91d43
asafgunay <asafgunay@gmail.com>
2019-06-22 14:47:50 +0300

    Navbar completed

--- OOPTut/src/OOPTut.Web.UI/ViewComponents/NavBarMenuViewComponent.cs
+++ OOPTut/src/OOPTut.Web.UI/ViewComponents/NavBarMenuViewComponent.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OOPTut.Application.NavbarService;
+using OOPTut.Core.Navbar;
+using OOPTut.Core.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +13,51 @@ namespace OOPTut.Web.UI.ViewComponents
     [ViewComponent]
     public class NavBarMenuViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly INavBarMenuItemService _navBarMenuItemService;
+        public NavBarMenuViewComponent(
+            UserManager<ApplicationUser> userManager,
+            INavBarMenuItemService navBarMenuItemService)
         {
+            _userManager = userManager;
+            _navBarMenuItemService = navBarMenuItemService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // navbar
+            List<NavBarMenuItem> model = new List<NavBarMenuItem>();
+            // getAllMenuItems
+            var navList = await _navBarMenuItemService.GetAll();
+            // anonym menu nesnelerini model listesine ekle
+            model.AddRange(navList.Where(x => x.IsAnonym).ToList());
             // client giris yapmış mı?
-            // giriş yaptıysa rolü var mı?
+            bool isAuthenticated = User.Identity.IsAuthenticated;
+
+            if (isAuthenticated)
+            {
+                // kişi giriş yapmış ise anonim olmayan ve rol koşulu bulunmayan menü nesnelerini modele ekle
+                model.AddRange(navList.Where(x => !x.IsAnonym && string.IsNullOrEmpty(x.Roles)).ToList());
+                // giriş yaptıysa rolü var mı?
+                var userName = User.Identity.Name;
+                var user = await _userManager.FindByNameAsync(userName);
+                List<string> userRoles = _userManager.GetRolesAsync(user).Result.ToList();
+                // eger kullanicin rolu varsa?
+                if (userRoles.Any())
+                {
+                    var roledNavList = navList.Where(x => !string.IsNullOrEmpty(x.Roles)).ToList();
+                    foreach (var item in roledNavList)
+                    {
+                        List<string> navRoles = item.Roles.Split(",").ToList();
+                        bool hasEqualRoles = navRoles.Intersect(userRoles).Count() > 0;
+                        if (hasEqualRoles)
+                            model.Add(item);
+                    }
+                }
+            }
 
             // yukarıdaki koşullara göre servisten gelen menü nesnelerini filtrele
             // ardından bunu view'e dön
-            return View();
+            return View(model);
         }
     }
 }
